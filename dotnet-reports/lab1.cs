310b24e97a03bc71ae8e9ccda856ae15dbacd8e9
notKuina <kuinaa00@gmail.com>
2024-08-19 06:37:55 +0545

    Update lab1.cs

--- dotnet-reports/lab1.cs
+++ dotnet-reports/lab1.cs
@@ -7,11 +7,8 @@ namespace lab1
     {
         static void Main(string[] args)
         {
-            // Prompt the user to enter their name
             Console.Write("Please enter your name: ");
             string userName = Console.ReadLine();
-
-            // Print the welcome message
             Console.WriteLine($"Welcome {userName}!");
             Console.ReadKey();
         }
