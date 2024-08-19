7308c0e4bada55077da8a10e190d6a7ba061c0cb
notKuina <kuinaa00@gmail.com>
2024-08-19 07:59:44 +0545

    Add files via upload

---
+++ dotnet-reports/lab3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace lab3
+{
+    class UserInfo
+    {/*
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Please enter your address: ");
+            string address = Console.ReadLine();
+
+            Console.Write("Please enter your email: ");
+            string email = Console.ReadLine();
+
+            Console.WriteLine("\nYour Details:");
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Address: {address}");
+            Console.WriteLine($"Email: {email}");
+
+            Console.ReadKey();
+        }
+   */ }
+}
